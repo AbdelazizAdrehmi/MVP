e928f40b1ad39a49c3b0f2fedefa2c0abcf25c62
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 10:59:49 +0100

    Income.js

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,1 +1,19 @@
+import React, { useState } from 'react';
 
+const Income = () => {
+  const [income, setIncome] = useState(0);
+
+  const handleIncomeChange = (e) => {
+    setIncome(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Income</h2>
+      <input type="number" value={income} onChange={handleIncomeChange} />
+      <p>Your income is: {income}</p>
+    </div>
+  );
+};
+
+export default Income;
