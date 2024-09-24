ced8181787bd6359e04772242c5172841a8070a2
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:36:10 +0100

    Income.js

--- src/models/Income.js
+++ src/models/Income.js
@@ -1,9 +1,13 @@
+// Import Mongoose
 const mongoose = require('mongoose');
 
+// Define the income schema
 const incomeSchema = new mongoose.Schema({
   amount: Number,
 });
 
+// Create the Income model
 const Income = mongoose.model('Income', incomeSchema);
 
+// Export the Income model
 module.exports = Income;
