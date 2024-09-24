7599246155a28d1f8757b50ef4c684077b0be055
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:00:30 +0100

    Transactions.js

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,1 +1,36 @@
+import React, { useState } from 'react';
 
+const Transactions = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const handleAddTransaction = (e) => {
+    e.preventDefault();
+    const transaction = {
+      name: e.target.transactionName.value,
+      amount: e.target.transactionAmount.value,
+    };
+    setTransactions([...transactions, transaction]);
+  };
+
+  return (
+    <div>
+      <h2>Transactions</h2>
+      <form onSubmit={handleAddTransaction}>
+        <label>Transaction Name:</label>
+        <input type="text" name="transactionName" />
+        <br />
+        <label>Transaction Amount:</label>
+        <input type="number" name="transactionAmount" />
+        <br />
+        <button type="submit">Add Transaction</button>
+      </form>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index}>{transaction.name}: {transaction.amount}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Transactions;
