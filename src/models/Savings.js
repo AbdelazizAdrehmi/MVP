30fa58a93d1580580acdcf0cee850e4f69cee6bd
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:02:03 +0100

    Savings.js

--- src/models/Savings.js
+++ src/models/Savings.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const savingsSchema = new mongoose.Schema({
+  amount: Number,
+});
+
+const Savings = mongoose.model('Savings', savingsSchema);
+
+module.exports = Savings;
