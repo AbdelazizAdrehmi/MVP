05c2411577ad5ffeb8273047038ae0388664b067
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:00:58 +0100

    Budget.js

--- src/models/Budget.js
+++ src/models/Budget.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const budgetSchema = new mongoose.Schema({
+  amount: Number,
+});
+
+const Budget = mongoose.model('Budget', budgetSchema);
+
+module.exports = Budget;
