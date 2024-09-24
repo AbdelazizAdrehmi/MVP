726118b8a0539f39a97a6b8d0812d52b0c9ac306
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:02:25 +0100

    Transactions.js

--- src/models/Transactions.js
+++ src/models/Transactions.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const transactionSchema = new mongoose.Schema({
+  name: String,
+  amount: Number,
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
