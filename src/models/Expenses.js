54cdd372e632ea2591c77ab408ad0fba27101664
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:35:49 +0100

    Expenses.js

--- src/models/Expenses.js
+++ src/models/Expenses.js
@@ -1,10 +1,14 @@
+// Import Mongoose
 const mongoose = require('mongoose');
 
+// Define the expenses schema
 const expenseSchema = new mongoose.Schema({
   name: String,
   amount: Number,
 });
 
+// Create the Expense model
 const Expense = mongoose.model('Expense', expenseSchema);
 
+// Export the Expense model
 module.exports = Expense;
