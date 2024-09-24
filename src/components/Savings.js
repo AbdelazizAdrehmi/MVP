cdc3a6ac5bfd20c089461806f86ff56c7fb92829
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:00:11 +0100

    Savings.js

--- src/components/Savings.js
+++ src/components/Savings.js
@@ -1,1 +1,19 @@
+import React, { useState } from 'react';
 
+const Savings = () => {
+  const [savings, setSavings] = useState(0);
+
+  const handleSavingsChange = (e) => {
+    setSavings(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Savings</h2>
+      <input type="number" value={savings} onChange={handleSavingsChange} />
+      <p>Your savings is: {savings}</p>
+    </div>
+  );
+};
+
+export default Savings;
