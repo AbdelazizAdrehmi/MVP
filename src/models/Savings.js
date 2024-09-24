a36c119fdbcb0b54ff69001b5bedf21ea19e6dd8
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-09-24 11:36:28 +0100

    Savings.js

--- src/models/Savings.js
+++ src/models/Savings.js
@@ -1,9 +1,13 @@
+// Import Mongoose
 const mongoose = require('mongoose');
 
+// Define the savings schema
 const savingsSchema = new mongoose.Schema({
   amount: Number,
 });
 
+// Create the Savings model
 const Savings = mongoose.model('Savings', savingsSchema);
 
+// Export the Savings model
 module.exports = Savings;
